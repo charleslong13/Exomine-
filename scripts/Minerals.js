3ee3fe2ca60df2e826fcaf0118fef97ef3077935
jkuns76 <jkoonman@gmail.com>
2021-10-28 11:08:54 -0500

    trying to fetch leah

--- scripts/Minerals.js
+++ scripts/Minerals.js
@@ -4,15 +4,17 @@ import { purchaseMineral } from "./database.js"
 import { getFacilityMinerals } from "./database.js"
 import { getColonyMinerals } from "./database.js"
 
+// const facilityMineralsTotal = getFacilityMinerals()
+// const colonyMineralsTotal = getColonyMinerals() 
 
 
 
+// // need to subtract minerals from the mineral total after clicking the button.
 
-// need to subtract minerals from the mineral total after clicking the button.
+// facilityMineralsTotal.quantity -=
 
-
-// need to add minerals to the colony total after clicking the button.
-
+// // need to add minerals to the colony total after clicking the button.
+// let totalMins = colonyMineralsTotal.quantity ++
 
 // after minerals are added and subtracted reset the carts state.
 
